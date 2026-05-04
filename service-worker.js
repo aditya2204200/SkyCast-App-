6cbb29616e4857c823e7a4cbfc0e125e4fcdfb48
Aditya Raj <ar25200312@gmail.com>
2026-05-04 16:55:06 +0530

    Update service worker cache version to v8

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,6 @@
-// const CACHE_NAME = "skycast-v2"; // 🔥 change karo
 
-// const urlsToCache = [
-//   "/SkyCast-App-/",
-//   "/SkyCast-App-/index.html",
-//   "/SkyCast-App-/style.css",
-//   "/SkyCast-App-/script.js",
-//   "/SkyCast-App-/logo.png",
-// ];
 
-// self.addEventListener("install", (event) => {
-//   self.skipWaiting(); // 🔥 new version activate fast
-// });
-// self.addEventListener("activate", (event) => {
-//   event.waitUntil(self.clients.claim());
-// });
-// self.addEventListener("message", (event) => {
-//   if (event.data.action === "skipWaiting") {
-//     self.skipWaiting();
-//   }
-// });
-
-const CACHE_NAME = "skycast-v7";
+const CACHE_NAME = "skycast-v8";
 
 self.addEventListener("install", (event) => {
   // ❌ यहाँ skipWaiting नहीं
