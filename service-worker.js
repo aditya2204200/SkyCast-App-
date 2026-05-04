629ab2063e2c2c69bc0a6d1b727f9f1d2bd6c1ae
Aditya Raj <ar25200312@gmail.com>
2026-05-04 16:41:43 +0530

    Update service worker to version v7

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,37 @@
-const CACHE_NAME = "skycast-v1";
+// const CACHE_NAME = "skycast-v2"; // 🔥 change karo
 
-const urlsToCache = [
-  "/SkyCast-App-/",
-  "/SkyCast-App-/index.html",
-  "/SkyCast-App-/style.css",
-  "/SkyCast-App-/script.js",
-  "/SkyCast-App-/logo.png",
-];
+// const urlsToCache = [
+//   "/SkyCast-App-/",
+//   "/SkyCast-App-/index.html",
+//   "/SkyCast-App-/style.css",
+//   "/SkyCast-App-/script.js",
+//   "/SkyCast-App-/logo.png",
+// ];
+
+// self.addEventListener("install", (event) => {
+//   self.skipWaiting(); // 🔥 new version activate fast
+// });
+// self.addEventListener("activate", (event) => {
+//   event.waitUntil(self.clients.claim());
+// });
+// self.addEventListener("message", (event) => {
+//   if (event.data.action === "skipWaiting") {
+//     self.skipWaiting();
+//   }
+// });
+
+const CACHE_NAME = "skycast-v7";
+
+self.addEventListener("install", (event) => {
+  // ❌ यहाँ skipWaiting नहीं
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("message", (event) => {
+  if (event.data.action === "skipWaiting") {
+    self.skipWaiting(); // ✅ only manual update
+  }
+});
