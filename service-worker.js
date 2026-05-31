43ada0aadb8ef18ebb21a0ec25448718148a37ce
Aditya Raj <ar25200312@gmail.com>
2026-05-31 09:24:43 +0530

    Upgrade service worker to v19 with notifications
    Updated service worker to version 19 with cache management and notification features.

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,47 @@
-const CACHE_NAME = "skycast-v18"; 
+const CACHE_NAME = "skycast-v19";
 
+// INSTALL
 self.addEventListener("install", (event) => {
-  self.skipWaiting(); //  auto activate new version
+  self.skipWaiting();
 });
 
+// ACTIVATE
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim()); //  old clients ko control le lo
+  event.waitUntil(
+    (async () => {
+      // OLD CACHE DELETE
+      const cacheNames = await caches.keys();
+
+      await Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        }),
+      );
+
+      // TAKE CONTROL
+      await self.clients.claim();
+
+      //  SHOW UPDATE NOTIFICATION
+      self.registration.showNotification("SkyCast Updated 🚀", {
+        body: "New AI Assistant feature added!",
+        icon: "logo.png",
+        badge: "logo.png",
+
+        vibrate: [200, 100, 200],
+
+        data: {
+          url: "/SkyCast-App-/",
+        },
+      });
+    })(),
+  );
+});
+
+// CLICK NOTIFICATION
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+
+  event.waitUntil(clients.openWindow(event.notification.data.url));
 });
